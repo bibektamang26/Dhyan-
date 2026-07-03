e70b5e8581a7c4098a8ec9e037b8029ebe7e113f
bibektamang26 <bibektamang953@gmail.com>
2026-07-03 23:32:50 +0545

    added footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
-console.log("DHYAN initialized.");
-
 fetch("../components/navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar").innerHTML = data;
+  });
+
+fetch("../components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
   });
